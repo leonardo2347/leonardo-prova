b44df48414488e8885a21033bd587077b280a0fa
Aluno <aluno@ifpi.edu.br>
2019-09-13 19:25:02 -0300

    Segunda parte da prova

--- primeira/models.py
+++ primeira/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    matricula = models.CharField(max_length=11)
+    idade = models.IntegerField()
+
+class Produto(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    categoria = models.CharField(max_length=11)
+    valor = models.CharField(max_length=1)
+
+class Carrinho_De_Compras(models.Model):
+    supermercado = models.CharField(max_length=50)
+    capacidade = models.CharField(max_length=11)
+    preco = models.IntegerField()
+    cliente = models.OneToOneField(Cliente, on_delete=models.CASCADE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
